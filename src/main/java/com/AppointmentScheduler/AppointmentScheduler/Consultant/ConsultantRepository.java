1b58d4082ff4371b026bf474d94531f715c13811
DilshanSands <dilshansandaruwands@gmail.com>
2023-09-04 13:27:14 +0530

    Initial

--- src/main/java/com/AppointmentScheduler/AppointmentScheduler/Consultant/ConsultantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.AppointmentScheduler.AppointmentScheduler.Consultant;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ConsultantRepository extends CrudRepository<Consultant, Integer>{
-
-}
